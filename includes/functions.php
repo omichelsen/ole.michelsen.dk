6641b6b19b7a120abecae45f321394d7354a028c
Ole Bjørn Michelsen <ole@michelsen.dk>
2014-04-04 15:35:39 +0200

    A lot of things.

--- includes/functions.php
+++ includes/functions.php
@@ -4,12 +4,12 @@ define("WWWROOT", getenv("DOCUMENT_ROOT"));
 
 function jQueryCdn()
 {
-	echo '<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>', "\n";
+	echo '<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>', "\n";
 }
 
 $jQueryUICdn = function()
 {
-	echo '<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>', "\n";
+	echo '<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>', "\n";
 }
 
 ?>
