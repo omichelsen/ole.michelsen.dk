48d10d59d4582541ad12c251e6077caf761dec2d
Ole Bjørn Michelsen <ole@michelsen.dk>
2014-09-26 15:02:58 +0200

    Refactored validation script.

--- public/scripts/validator.js
+++ public/scripts/validator.js
@@ -1,65 +1,53 @@
-// Set var for no. of active timers
 var activeTimers = 0;
 var timerIntervalId = 0;
 
 $(document).ready(function() {
-
-	$('#validator').submit(function() {
-
-		// Disable the submit button
+	$('#validator').submit(function(event) {
 		$('input[type=submit]').prop('disabled', true);
 
-		// Insert sitemap status message (overwrite previous results)
-		$("#results").html('<tr><td class="c" colspan="3">Processing sitemap...<br /><img src="http://cdn.ole.michelsen.dk/images/ajax-loader.gif" height="11" width="16" /></td></tr>');
+		$('#results').html('<tr><td class="c" colspan="3">Processing sitemap...<br /><img src="/images/loading.gif" height="11" width="16" /></td></tr>');
 
-		$.getJSON("/api/validator-sitemap.php", {"uri":$("#sitemapuri").val()}, function(data) {
-				if (data.length > 0) {
-					var delay = 2000;
-					var out = '';
-					var requestLimit = 1000;
+		$.getJSON('/api/validator-sitemap.php', {"uri": $("#sitemapuri").val()}, bindSitemapResponse);
 
-					$.each(data, function(index,value) {
-
-						out += '<tr' + (index % 2 == 1 ? ' class="alt">' : '>')
-							+  '<td class="r">' + (index+1) + ':</td>'
-							+  '<td>' + value + '</td>'
-							+  '<td class="c" id="loading' + index + '">'
-							+	(index < requestLimit ? '<img src="http://cdn.ole.michelsen.dk/images/ajax-loader.gif" height="11" width="16" />' : 'Deferred')
-							+  '</td></tr>';
-
-						if (index < requestLimit) {
-							// Request the validation service for each uri with specified delay
-							setTimeout("validate(" + index + ", '" + value + "')", delay * index);
-
-							// We have added a new timer
-							activeTimers++;
-						}
-					});
-
-					$("#results").html(out);
-
-				} else {
-
-					// No data was returned, so display error message and reenable submit for retry
-					$("#results td:first").html('<span style="color:#B00">Sitemap could not be parsed - wrong URI?</span>');
-					$('input[type=submit]').prop('disabled', false);
-
-				}
-			}
-		);
-
-		return false;
+		event.preventDefault();
 	});
 });
 
-function validate(index,uri) {
+function bindSitemapResponse(data) {
+	if (!data.length) {
+		// No data was returned, so display error message and reenable submit for retry
+		$("#results td:first").html('<span style="color:#B00">Sitemap could not be parsed - wrong URI?</span>');
+		$('input[type=submit]').prop('disabled', false);
+	}
 
-	$.getJSON(
-		  "/api/validator-proxy.php"
-		, { "uri": uri }
-		, function(data) {
+	var delay = 2000;
+	var out = '';
+	var requestLimit = 1000;
 
-			$("#loading"+index).html(
+	$.each(data, function(index, value) {
+		out += '<tr' + (index % 2 == 1 ? ' class="alt">' : '>')
+			+  '<td class="r">' + (index+1) + ':</td>'
+			+  '<td>' + value + '</td>'
+			+  '<td class="c" id="loading' + index + '">'
+			+	(index < requestLimit ? '<img src="/images/loading.gif" height="11" width="16" />' : 'Deferred')
+			+  '</td></tr>';
+
+		if (index < requestLimit) {
+			// Request the validation service for each uri with specified delay
+			setTimeout(validate.bind(this, index, value), delay * index);
+
+			// We have added a new timer
+			activeTimers++;
+		}
+	});
+
+	$("#results").html(out);
+}
+
+function validate(index, uri) {
+	$.getJSON('/api/validator-proxy.php', { 'uri': uri },
+		function (data) {
+			$('#loading' + index).html(
 				'<a href="http://validator.w3.org/check?uri=' + encodeURIComponent(uri) + '" target="_blank" style="color:' +
 				(data.valid == 'Passed' ? '#006400' : '#B00') + '">' +
 				(data.valid || data.error) +
@@ -71,11 +59,10 @@ function validate(index,uri) {
 			checkTimers();
 		}
 	);
-
 }
 
 function checkTimers() {
-	if(activeTimers < 1) {
+	if (activeTimers < 1) {
 		$('input[type=submit]').removeAttr('disabled');
 		clearInterval(timerIntervalId);
 	}
