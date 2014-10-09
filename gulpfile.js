43e8473d69e234f0e99a455f107cdb6e5f860408
Ole Bjørn Michelsen <ole@michelsen.dk>
2014-10-09 13:06:58 +0200

    Added gulp task for reading exif data for travel map.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,18 @@
-var gulp = require('gulp');
-var ftp = require('gulp-ftp');
-var shell = require('gulp-shell');
+var gulp = require('gulp'),
+    data = require('gulp-data'),
+    exif = require('gulp-exif'),
+    extend = require('gulp-extend'),
+    ftp = require('gulp-ftp'),
+    shell = require('gulp-shell');
+
+function gpsDecimal(direction, degrees, minutes, seconds) {
+    var d = degrees + minutes / 60 + seconds / (60 * 60);
+    return (direction === 'S' || direction === 'W') ? d *= -1 : d;
+}
+
+function roundDecimal(dec) {
+    return parseFloat(dec.toFixed(4));
+}
 
 gulp.task('harp', shell.task([
     'harp compile'
@@ -16,4 +28,26 @@ gulp.task('ftp', ['harp'], function () {
         }));
 });
 
-gulp.task('default', ['harp', 'ftp']);
+gulp.task('exif', function () {
+    return gulp.src('./public/photos/map/*.jpg')
+        .pipe(exif())
+        .pipe(data(function (file) {
+            var filename = file.path.substring(file.path.lastIndexOf('/') + 1),
+                calcLat = gpsDecimal.bind(null, file.exif.gps.GPSLatitudeRef),
+                calcLng = gpsDecimal.bind(null, file.exif.gps.GPSLongitudeRef),
+                img = {
+                    lat: roundDecimal(calcLat.apply(null, file.exif.gps.GPSLatitude)),
+                    lng: roundDecimal(calcLng.apply(null, file.exif.gps.GPSLongitude)),
+                    width: file.exif.exif.ExifImageWidth,
+                    height: file.exif.exif.ExifImageHeight
+                },
+                data = {};
+            data[filename] = img;
+            file.contents = new Buffer(JSON.stringify(data));
+        }))
+        .pipe(extend('exif.json'))
+        .pipe(gulp.dest('./public/photos'));
+});
+
+// gulp.task('default', ['harp', 'ftp']);
+gulp.task('default', ['exif']);
