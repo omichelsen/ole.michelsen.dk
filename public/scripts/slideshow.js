faffa6aedd93f2025eebb53811275715cfd42613
Ole Bjørn Michelsen <ole@michelsen.dk>
2015-03-19 19:09:10 +0100

    Ensure that animation does not run on load.

--- public/scripts/slideshow.js
+++ public/scripts/slideshow.js
@@ -15,14 +15,13 @@
 
     function SlideShow(selector, options) {
         options = options || {};
-        this.options = {
-            timeout: options.timeout || 5000,
-            speed:   options.speed   || 2000
-        };
+        options.timeout = options.timeout || 5000;
 
-        var elm = document.querySelector(selector),
-            index = 0;
+        var index = 0;
+            elm = document.querySelector(selector);
+            elm.classList.add('preload');
 
+        // Create caption elements from image properties
         for (var i = 0; i < elm.childNodes.length; i++) {
             var elmChild = elm.childNodes[i],
                 elmImg = elmChild.querySelector('img'),
@@ -36,13 +35,20 @@
             elmChild.insertBefore(elmCaption, elmChild.firstChild);
         }
 
+        // Show the first slide
         elm.firstChild.classList.add('show-animation');
 
+        // Remove preload class to enable transition animations
+        setTimeout(function () {
+            elm.classList.remove('preload');
+        }, 0);
+
+        // Start the slidehshow
         setInterval(function () {
             elm.childNodes[index].classList.remove('show-animation');
             index = (index + 1) % elm.childNodes.length;
             elm.childNodes[index].classList.add('show-animation');
-        }.bind(this), this.options.timeout);
+        }.bind(this), options.timeout);
     }
 
     return SlideShow;
