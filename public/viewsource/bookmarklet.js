74a5be45b74d15dc5e886e48b62acfe9ce87fb5a
Ole Bjørn Michelsen <ole@michelsen.dk>
2015-12-08 14:19:21 +0100

    Attempt to fix viewsource bookmarklet.

--- public/viewsource/bookmarklet.js
+++ public/viewsource/bookmarklet.js
@@ -1,14 +1,15 @@
 javascript:(function(){
-	var w = window.open('about:blank'), 
-		s = w.document;
-	s.write('<!DOCTYPE html><html><head><title>Loading Source</title></head><body></body></html>');
-	s.close();
-	var f = s.body.appendChild(s.createElement('form'));
-		f.setAttribute('method','post');
-		f.setAttribute('action','http://ole.michelsen.dk/viewsource/?uri='+location.href);
-	var i = f.appendChild(s.createElement('input'));
-		i.setAttribute('type','hidden');
-		i.setAttribute('name','DOM');
-		i.setAttribute('value',encodeURIComponent(document.documentElement.innerHTML));
-	f.submit();
+    var w = window.open('about:blank'),
+        d = w.document;
+    d.write('<!DOCTYPE html><html><head><title>Loading Source</title></head><body></body></html>');
+    d.close();
+    var f = d.createElement('form');
+        f.setAttribute('method','post');
+        f.setAttribute('action','https://ole.michelsen.dk/viewsource/?uri='+encodeURIComponent(location.href));
+    var i = d.createElement('input');
+        i.setAttribute('type','hidden');
+        i.setAttribute('name','DOM');
+        i.setAttribute('value',encodeURIComponent(document.documentElement.innerHTML));
+    d.body.appendChild(f).appendChild(i);
+    f.submit();
 })();
