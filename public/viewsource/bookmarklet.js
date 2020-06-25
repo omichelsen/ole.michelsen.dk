7c208ceca178b60ef1d9fa5c190aa45ab843d8f0
Ole Bjørn Michelsen <ole@michelsen.dk>
2020-06-25 14:51:58 -0700

    Update the View Source article with working client-side bookmarklet.

--- public/viewsource/bookmarklet.js
+++ public/viewsource/bookmarklet.js
@@ -1,15 +1,21 @@
-javascript:(function(){
-    var w = window.open('about:blank'),
-        d = w.document;
-    d.write('<!DOCTYPE html><html><head><title>Loading Source</title></head><body></body></html>');
-    d.close();
-    var f = d.createElement('form');
-        f.setAttribute('method','post');
-        f.setAttribute('action','https://ole.michelsen.dk/viewsource/?uri='+encodeURIComponent(location.href));
-    var i = d.createElement('input');
-        i.setAttribute('type','hidden');
-        i.setAttribute('name','DOM');
-        i.setAttribute('value',encodeURIComponent(document.documentElement.innerHTML));
-    d.body.appendChild(f).appendChild(i);
-    f.submit();
-})();
+javascript:(() => {
+  var e = 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.20.0',
+  t = window.open('about:blank').document;
+  t.write(\`
+          <!DOCTYPE html>
+          <html>
+            <head>
+              <title>Source of ${location.href}</title>
+              <link rel=stylesheet href=${e}/themes/prism.min.css>
+            </head>
+            <body bgcolor=#f5f2f0>
+              <script src=${e}/components/prism-core.min.js></script>
+              <script src=${e}/plugins/autoloader/prism-autoloader.min.js></script>
+              <script src=${e}/plugins/autolinker/prism-autolinker.min.js></script>
+            </body>
+          </html>\`),
+        t.close();
+        var r = t.body.appendChild(t.createElement('pre')).appendChild(t.createElement('code'));
+        r.className = 'language-html',
+        r.appendChild(t.createTextNode(document.documentElement.outerHTML))
+      })();
