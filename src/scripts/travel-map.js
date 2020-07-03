90537be2a8fa4dc07b68319d4896d259fa232255
Ole Bjørn Michelsen <ole@michelsen.dk>
2020-07-03 09:36:32 -0700

    Add svg marker to travel map and update libs.

--- src/scripts/travel-map.js
+++ src/scripts/travel-map.js
@@ -1,81 +1,87 @@
+// https://developers.google.com/maps/documentation/javascript/marker-clustering
+
 function initialize() {
-    var opt = {
-        zoom: 2,
-        center: new google.maps.LatLng(40.416698, -3.700354),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var elmMap = document.getElementById('map-canvas');
-    var map = new google.maps.Map(elmMap, opt);
+  var opt = {
+    zoom: 2,
+    center: new google.maps.LatLng(40.416698, -3.700354),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    styles: [{ stylers: [{ saturation: -100 }] }],
+  }
+  var elmMap = document.getElementById('map-canvas')
+  var map = new google.maps.Map(elmMap, opt)
 
-    var marker;
-    for (var img in locations) {
-        marker = new google.maps.Marker({
-            position: new google.maps.LatLng(locations[img].lat, locations[img].lng),
-            map: map
-        });
-        // Overwrite image obj with Marker object instead
-        locations[img] = marker;
-    }
+  var marker
+  for (var img in locations) {
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(locations[img].lat, locations[img].lng),
+      map: map,
+    })
+    // Overwrite image obj with Marker object instead
+    locations[img] = marker
+  }
 
-    // Cluster adjacent markers depending on zoom level
-    var markerStyle = {
-        url: '/images/mapmarker.png',
-        width: 20,
-        height: 20,
-        opt_textSize: 9
-    };
-    var locationsArr = Object.keys(locations).map(function (value) {
-        return locations[value];
-    });
-    var mc = new MarkerClusterer(map, locationsArr, {
-        gridSize: 20,
-        maxZoom: 4,
-        styles: [markerStyle, markerStyle]
-    });
+  // Cluster adjacent markers depending on zoom level
+  var markerStyle = {
+    url: '/images/mapmarker.svg',
+    width: 20,
+    height: 20,
+    opt_textSize: 9,
+  }
+  var markers = Object.keys(locations).map((l) => locations[l])
+  var mc = new MarkerClusterer(map, markers, {
+    gridSize: 20,
+    maxZoom: 4,
+    styles: [markerStyle, markerStyle],
+  })
 
-    function scrollTo(element, to, duration) {
-        if (duration < 0) return;
-        var difference = to - element.scrollTop;
-        var perTick = difference / duration * 10;
+  function scrollTo(element, to, duration) {
+    if (duration < 0) return
+    var difference = to - element.scrollTop
+    var perTick = (difference / duration) * 10
 
-        setTimeout(function() {
-            element.scrollTop = element.scrollTop + perTick;
-            if (element.scrollTop === to) return;
-            scrollTo(element, to, duration - 10);
-        }, 10);
-    }
+    setTimeout(function () {
+      element.scrollTop = element.scrollTop + perTick
+      if (element.scrollTop === to) return
+      scrollTo(element, to, duration - 10)
+    }, 10)
+  }
 
-    function imageClickHandler(event) {
-        event.preventDefault();
+  function imageClickHandler(event) {
+    event.preventDefault()
 
-        // Cancel any active animations
-        if (marker) marker.setAnimation(null);
+    // Cancel any active animations
+    if (marker) marker.setAnimation(null)
 
-        // Activate new pin
-        var path = this.dataset.echo;
-        var filename = decodeURIComponent(path.substring(path.lastIndexOf('/') + 1));
-        marker = locations[filename];
-        marker.setAnimation(google.maps.Animation.BOUNCE);
-        map.panTo(marker.getPosition());
+    // Activate new pin
+    var path = this.dataset.echo
+    var filename = decodeURIComponent(path.substring(path.lastIndexOf('/') + 1))
+    marker = locations[filename]
+    marker.setAnimation(google.maps.Animation.BOUNCE)
+    map.panTo(marker.getPosition())
 
-        // Set zoom level
-        if (map.getZoom() < 5 || map.getZoom() > 15)
-            map.setZoom(5);
+    // Set zoom level
+    if (map.getZoom() < 5 || map.getZoom() > 15) map.setZoom(5)
 
-        scrollTo(document.documentElement.scrollTo ? document.documentElement : document.body, elmMap.offsetTop - 3, 1000);
-    }
+    scrollTo(
+      document.documentElement.scrollTo
+        ? document.documentElement
+        : document.body,
+      elmMap.offsetTop - 3,
+      1000
+    )
+  }
 
-    var elmImages = document.querySelectorAll('.images .image-fixed');
-    for (var i = 0; i < elmImages.length; i++) {
-        elmImages[i].addEventListener('click', imageClickHandler, false);
-    }
+  var elmImages = document.querySelectorAll('.images .image-fixed')
+  for (var i = 0; i < elmImages.length; i++) {
+    elmImages[i].addEventListener('click', imageClickHandler, false)
+  }
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', initialize)
 
 // Init Masonry
 var msnry = new Masonry('.images', {
-    itemSelector: '.image-fixed',
-    columnWidth: 106,
-    resizable: false
-});
+  itemSelector: '.image-fixed',
+  columnWidth: 106,
+  resizable: false,
+})
